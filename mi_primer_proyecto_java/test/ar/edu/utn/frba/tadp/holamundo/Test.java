a5fe94d97f69d376e3a5da1cdae6aa61a92af1ce
Leandro Laiso <leandrolaisogp@gmail.com>
2026-04-28 15:01:13 -0300

    Se hace uso de una ArrayList en Test para nombrables

--- mi_primer_proyecto_java/test/ar/edu/utn/frba/tadp/holamundo/Test.java
+++ mi_primer_proyecto_java/test/ar/edu/utn/frba/tadp/holamundo/Test.java
@@ -1,16 +1,27 @@
 package ar.edu.utn.frba.tadp.holamundo;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 import ar.edu.utn.frba.tadp.entes.Nombrable;
 import ar.edu.utn.frba.tadp.entes.Persona;
 
 public class Test {
 	
 	public static void main(String[] args) {
+		Collection<Nombrable> nombrables = getNombrables();
 		Recepcionista recepcionista = makeRecepcionista();
-		Nombrable nombrable = new Mundo();
-		recepcionista.saludar(nombrable);
-		nombrable = new Persona("José");
-		recepcionista.saludar(nombrable);
+		for(Nombrable nombrable : nombrables) {
+			recepcionista.saludar(nombrable);
+		}
+	}
+	
+	private static Collection<Nombrable> getNombrables() {
+		Collection<Nombrable> nombrables = new ArrayList<Nombrable>();
+		nombrables.add(new Mundo());
+		nombrables.add(new Persona("Juan"));
+		nombrables.add(new Persona("José"));
+		return nombrables;
 	}
 	
 	private static Recepcionista makeRecepcionista() {
