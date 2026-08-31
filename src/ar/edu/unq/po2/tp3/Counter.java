972f692cb6b924435ab7413a9cdce281996a3c06
Leandro Laiso <leandrolaisogp@gmail.com>
2026-08-31 13:07:13 -0300

    Ejercicio 1 finalizado + Tests

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -5,14 +5,20 @@ import java.util.List;
 
 public class Counter {
 
-    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> numbers = new ArrayList<Integer>();
 
+    /**
+     * Agrega un número al Counter
+     * @param number el número a agregar.
+     */
     public void addNumber(int number) {
         numbers.add(number);
     }
 
-
-    public int getEvenOcurrences() {
+    /**
+     * @return La cantidad de números pares existentes en el Counter
+     */
+    public int getEvenOccurrences() {
         int counter = 0;
         for (int n: numbers) {
             if (n % 2 == 0) {
@@ -22,8 +28,31 @@ public class Counter {
         return counter;
     }
 
-    public int getOddOcurrences() {
-        return -1; // TODO
+    /**
+     * @return La cantidad de números impares existentes en el Counter
+     */
+    public int getOddOccurrences() {
+        int counter = 0;
+        for (int n: numbers) {
+            if (n % 2 == 1) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    /**
+     * @param x el número del que se verifica la cantidad de múltiplos
+     * @return La cantidad de números que son múltiplos de x existentes en el Counter
+     */
+    public int getMultiplesOf(int x) {
+        int counter = 0;
+        for (int n: numbers) {
+            if (n % x == 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
 }
--- test/ar/edu/unq/po2/tp3/CounterTest.java
+++ test/ar/edu/unq/po2/tp3/CounterTest.java
@@ -38,8 +38,35 @@ public class CounterTest {
     @Test
     public void testEvenNumbers() {
         // Getting the even occurrences
-        int amount = counter.getEvenOcurrences();
+        int amount = counter.getEvenOccurrences();
         // I check the amount is the expected one
-        assertEquals(amount, 1);
+        assertEquals(1, amount);
+    }
+
+    /**
+     * Verifica la cantidad de impares
+     */
+    @Test
+    public void testOddNumbers() {
+        // Getting the odd occurrences
+        int amount = counter.getOddOccurrences();
+        // I check the amount is the expected one
+        assertEquals(9, amount);
+    }
+
+    /**
+     * Verifica la cantidad de multiplos de un cierto número
+     */
+    @Test
+    public void testMultiples() {
+        // Getting the multiples
+        int multiplesOf1 = counter.getMultiplesOf(1);
+        int multiplesOf2 = counter.getMultiplesOf(2);
+        int multiplesOf3 = counter.getMultiplesOf(3);
+
+        // Checking the amounts are the expected ones
+        assertEquals(10, multiplesOf1);
+        assertEquals(1, multiplesOf2);
+        assertEquals(2, multiplesOf3);
     }
 }
