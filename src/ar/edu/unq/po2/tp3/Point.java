9c114814552d37e29c2e5504aa7dddb40e539c81
Leandro Laiso <leandrolaisogp@gmail.com>
2026-08-31 17:05:33 -0300

    Ejercicio 8 (Documentación)

--- src/ar/edu/unq/po2/tp3/Point.java
+++ src/ar/edu/unq/po2/tp3/Point.java
@@ -2,17 +2,29 @@ package ar.edu.unq.po2.tp3;
 
 public class Point {
 
+    // Atributos
     private int x;
     private int y;
 
+    // Constructores
+
+    /**
+     * Crea una nueva instancia de Point inicializada con las coordenadas por defecto x = 0 e y = 0.
+     */
     public Point() {
         this.setXY(0, 0);
     }
 
+    /**
+     * Crea una nueva instancia de point inicializada con las coordenadas recibidas por parámetro.
+     * @param x
+     * @param y
+     */
     public Point(int x, int y) {
         this.setXY(x, y);
     }
 
+    // Getters y Setters
     public int getX() {
         return x;
     }
@@ -34,6 +46,13 @@ public class Point {
         this.setY(y);
     }
 
+    // Metodos
+
+    /**
+     *
+     * @param p El Point a sumar
+     * @return una nueva instancia de Point cuyas coordenadas resultan de sumar las coordenadas correspondientes del Point actual con las de p.
+     */
     public Point add(Point p) {
         return new Point(this.getX() + p.getX(), this.getY() + p.getY());
     }
